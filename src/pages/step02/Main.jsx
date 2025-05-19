84acd87db64b6ad9f4f722a2d8b39796bde295ce
nohssam <nohssam73@gmail.com>
2025-05-19 11:16:40 +0900

    0519-01

--- src/pages/step02/Main.jsx
+++ src/pages/step02/Main.jsx
@@ -17,6 +17,10 @@ import Event02 from "../step08/Event02";
 import Event03 from "../step08/Event03";
 import Event04 from "../step08/Event04";
 import InputExam from "../step09/InputExam";
+// import CountEx from "../step10/CountEx";
+import CountEx2 from "../step10/CountEx2";
+import CountEx3 from "../step10/CountEx3";
+import CountEx4 from "../step10/CountEx4";
 import Counter from "./Counter";
 
 function Main(props){
@@ -44,6 +48,9 @@ function Main(props){
             <Event03 />
             <Event04 />
             <InputExam />
+            <CountEx2 />
+            <CountEx3 />
+            <CountEx4 />
         </main>
     );
 };
---
+++ src/pages/step10/CountEx2.jsx
@@ -0,0 +1,19 @@
+import { Button } from "@mui/material";
+import { useState } from "react";
+
+export default function CountEx2(){
+    // 리엑트에서 화면이 변경(재 렌더링)하기 위해서는 상태값이 변경되었다고 
+    // 리엑트에게 알려줘야 한다. (상태관리 => useState)
+    
+    //    [변수이름, setter] = useState(초기값)
+    const [count, setCount] = useState(5) ;
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+    return(
+        <>
+             <h2>Count : {count} </h2>
+            <Button variant="outlined" onClick={increment}> + </Button>
+            <Button variant="outlined" onClick={decrement}> - </Button>
+        </>
+    );
+}
