e32179c68197d60e86892f892973bdf5af444532
nohssam <nohssam73@gmail.com>
2025-05-20 10:02:54 +0900

    0520-01 useCallback

--- src/pages/step02/Main.jsx
+++ src/pages/step02/Main.jsx
@@ -1,44 +1,47 @@
-import List01 from "../step03/List01";
-import List02 from "../step03/List02";
-import Profile from "../step03/Profile";
-import CommentList from "../step04/CommentList";
-import FilteredCommandList2 from "../step05/FilteredCommandList2";
-import FilteredCommandList from "../step05/FilteredCommentList";
-import FindAdminComment from "../step05/FindAdminComment";
-import FindAdminComment2 from "../step05/FindAdminComment2";
-import ReduceComment from "../step05/ReduceComment";
-import SomeEveryCheck from "../step05/SomeEveryCheck";
-import IfExam01 from "../step06/IfExam01";
-import IfExam02List from "../step06/IfExam02List";
-import TextList1 from "../step07/TextList1";
-import TextList2 from "../step07/TextList2";
-import Event01 from "../step08/Event01";
-import Event02 from "../step08/Event02";
-import Event03 from "../step08/Event03";
-import Event04 from "../step08/Event04";
-import InputExam from "../step09/InputExam";
-// import CountEx from "../step10/CountEx";
-import CountEx2 from "../step10/CountEx2";
-import CountEx3 from "../step10/CountEx3";
-import CountEx4 from "../step10/CountEx4";
-import CountEx5 from "../step10/CountEx5";
-import ProfileEx from "../step11/ProfileEx";
-import TempEx from "../step12/TempEx";
-import FormEx from "../step13/FormEx";
-import EffectTest01 from "../step14/EffectTest01";
-import EffectTest02 from "../step14/EffectTest02";
-import EffectTest03 from "../step14/EffectTest03";
-import EffectTest04 from "../step14/EffectTest04";
-import MemoTest01 from "../step15/MemoTest01";
-import MemoTest02 from "../step15/MemoTest02";
-import Counter from "./Counter";
+// import List01 from "../step03/List01";
+// import List02 from "../step03/List02";
+// import Profile from "../step03/Profile";
+// import CommentList from "../step04/CommentList";
+// import FilteredCommandList2 from "../step05/FilteredCommandList2";
+// import FilteredCommandList from "../step05/FilteredCommentList";
+// import FindAdminComment from "../step05/FindAdminComment";
+// import FindAdminComment2 from "../step05/FindAdminComment2";
+// import ReduceComment from "../step05/ReduceComment";
+// import SomeEveryCheck from "../step05/SomeEveryCheck";
+// import IfExam01 from "../step06/IfExam01";
+// import IfExam02List from "../step06/IfExam02List";
+// import TextList1 from "../step07/TextList1";
+// import TextList2 from "../step07/TextList2";
+// import Event01 from "../step08/Event01";
+// import Event02 from "../step08/Event02";
+// import Event03 from "../step08/Event03";
+// import Event04 from "../step08/Event04";
+// import InputExam from "../step09/InputExam";
+// // import CountEx from "../step10/CountEx";
+// import CountEx2 from "../step10/CountEx2";
+// import CountEx3 from "../step10/CountEx3";
+// import CountEx4 from "../step10/CountEx4";
+// import CountEx5 from "../step10/CountEx5";
+// import ProfileEx from "../step11/ProfileEx";
+// import TempEx from "../step12/TempEx";
+// import FormEx from "../step13/FormEx";
+// import EffectTest01 from "../step14/EffectTest01";
+// import EffectTest02 from "../step14/EffectTest02";
+// import EffectTest03 from "../step14/EffectTest03";
+// import EffectTest04 from "../step14/EffectTest04";
+// import MemoTest01 from "../step15/MemoTest01";
+// import MemoTest02 from "../step15/MemoTest02";
+// import Counter from "./Counter";
+
+import NonUseCallBackEx from "../step16/NonUseCallBackEx";
+import UseCallBackEx from "../step16/UseCallBackEx";
 
 function Main(props){
     return(
         // 태그의 첫글자가 소문자이면 일반적인 html 태그, 대문자이면 컴포넌트
         <main>
             <h1>WelComa React</h1>
-            <Counter />
+            {/* <Counter />
             <List01 />
             <List02 />
             <Profile />
@@ -70,7 +73,11 @@ function Main(props){
             <EffectTest03 />
             <EffectTest04 />
             <MemoTest01 />
-            <MemoTest02 />
+            <MemoTest02 /> 
+            <NonUseCallBackEx />
+            <UseCallBackEx />
+            */}
+
         </main>
     );
 };
